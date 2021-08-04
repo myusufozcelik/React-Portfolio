b1b56ed9ebce2036cfdd4ac1092c031bd60baa89
myusuf <myusufozcelik96@gmail.com>
2021-08-04 19:07:34 +0300

    responsive, works,  testimonials added

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,31 @@
-import "./contact.scss"
+import { useState } from "react";
+import "./contact.scss";
 
 function Contact() {
-    return (
-        <div className="contact" id="contact">
-            contact
-        </div>
-    )
+
+    const [message, setMessage] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setMessage(true);
+    }
+
+  return (
+    <div className="contact" id="contact">
+      <div className="left">
+        <img src="assets/shake.svg" alt="" />
+      </div>
+      <div className="right">
+        <h2>Contact</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+          {message && <span>Thanks, I'll reply A$AP :)</span>}
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
